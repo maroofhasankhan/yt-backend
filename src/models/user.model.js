ad14cb368b601e238b6f157476d20cc83733f0ee
maroofhasankhan <8009129615s@gmail.com>
2024-10-09 12:33:26 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,88 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt  from "jsonwebtoken";
+
+
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  coverImage: {
+    type: String,
+  },
+  watchHistory: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+  ],
+  password: {
+    type: String,
+    required: [true, 'password is required'],
+  },
+  refreshToken: {
+    type: String,
+  }
+},
+{timestamp: true});
+
+
+// hashing the password before saving it to the database
+userSchema.pre("save",async function(req,res,next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+
+// matching the hash pass enterd by the user with database password
+userSchema.method.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.method.generateAccessToken = async function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+     },
+     process.env.ACCESS_TOKEN_SECRET,
+     {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+     }
+    )
+}
+userSchema.method.generateRefreshToken = async function(){
+    return jwt.sign({
+        _id:this._id,
+     },
+     process.env.REFRESH_TOKEN_SECRET,
+     {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+     }
+    )
+}
+
+export const User = mongoose.model("User", userSchema);
